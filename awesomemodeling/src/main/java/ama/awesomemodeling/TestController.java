d4c33dbc403bae9f516dc2ae86b1e4b836e1c04f
Alberto Basaglia <alberto.basaglia17@gmail.com>
2024-05-13 15:10:40 +0200

    add docker compose and rabbit

---
+++ awesomemodeling/src/main/java/ama/awesomemodeling/TestController.java
@@ -0,0 +1,29 @@
+package ama.awesomemodeling;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+@RestController
+public class TestController {
+    private final static String QUEUE_NAME = "hello";
+
+    @GetMapping("/")
+    String test() {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("rabbit");
+        try (Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel()) {
+                channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+                String message = "Hello World!";
+                channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+                System.out.println(" [x] Sent '" + message + "'");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return "Hello World!";
+    }
+}
