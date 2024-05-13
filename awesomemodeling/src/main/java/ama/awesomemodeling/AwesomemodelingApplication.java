baa199ba8e793be8d5edb84839d338ed9ee5f8a0
Alberto Basaglia <alberto.basaglia17@gmail.com>
2024-05-13 13:51:13 +0200

    add basic project structure

---
+++ awesomemodeling/src/main/java/ama/awesomemodeling/AwesomemodelingApplication.java
@@ -0,0 +1,13 @@
+package ama.awesomemodeling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomemodelingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomemodelingApplication.class, args);
+	}
+
+}
---
+++ awesomemodeling/src/test/java/ama/awesomemodeling/AwesomemodelingApplicationTests.java
@@ -0,0 +1,13 @@
+package ama.awesomemodeling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomemodelingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
