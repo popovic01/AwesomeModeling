4916eaadc6f801d92128ac6974612187d6462448
Alberto Basaglia <alberto.basaglia17@gmail.com>
2024-06-21 16:47:50 +0200

    cleanup q1 controller

--- awesomemodeling/src/main/java/ama/awesomemodeling/controllers/QOneController.java
+++ awesomemodeling/src/main/java/ama/awesomemodeling/controllers/QOneController.java
@@ -2,9 +2,18 @@ package ama.awesomemodeling.controllers;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Formatter;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
+import java.util.TreeSet;
 
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,43 +21,34 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.elasticsearch.client.RestClient;
-import org.apache.http.HttpHost;
-import org.apache.http.Header;
-import org.apache.http.message.BasicHeader;
 
-import co.elastic.clients.transport.ElasticsearchTransport;
-import co.elastic.clients.transport.rest_client.RestClientTransport;
-import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import co.elastic.clients.json.jackson.JacksonJsonpMapper;
-import co.elastic.clients.elasticsearch.core.SearchResponse;
-import co.elastic.clients.elasticsearch.core.search.Hit;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
+import ama.awesomemodeling.dtos.QOneCreateDTO;
 import ama.awesomemodeling.entities.Article;
 import ama.awesomemodeling.entities.QOne;
 import ama.awesomemodeling.entities.QTwoTopics;
-import ama.awesomemodeling.dtos.QOneCreateDTO;
 import ama.awesomemodeling.enums.QOneStatus;
 import ama.awesomemodeling.repositories.ControlRepository;
-
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.Channel;
-
-import cc.mallet.pipe.*;
+import cc.mallet.pipe.CharSequence2TokenSequence;
+import cc.mallet.pipe.Pipe;
+import cc.mallet.pipe.SerialPipes;
+import cc.mallet.pipe.TokenSequence2FeatureSequence;
+import cc.mallet.pipe.TokenSequenceLowercase;
+import cc.mallet.pipe.TokenSequenceRemoveStopwords;
 import cc.mallet.pipe.iterator.StringArrayIterator;
 import cc.mallet.topics.ParallelTopicModel;
-import cc.mallet.types.*;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Formatter;
-import java.util.Locale;
-import java.util.Iterator;
-import java.util.TreeSet;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import cc.mallet.types.Alphabet;
+import cc.mallet.types.IDSorter;
+import cc.mallet.types.InstanceList;
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.elasticsearch.core.search.Hit;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
 
 @RequestMapping("/q1")
 @RestController
@@ -59,7 +59,7 @@ public class QOneController {
     private ControlRepository repo;
 
     @PostMapping("")
-    String post(@RequestBody QOneCreateDTO dto) {
+    ResponseEntity<QOne> post(@RequestBody QOneCreateDTO dto) {
 
         QOne qone = new QOne();
 
@@ -75,49 +75,66 @@ public class QOneController {
         factory.setHost("rabbit");
 
         try (Connection connection = factory.newConnection();
-            Channel channel = connection.createChannel()) {
+                Channel channel = connection.createChannel()) {
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
             // Integer id = saved.getId();
             // byte[] idbytes = ByteBuffer.allocate(4).putInt(id).array();
             channel.basicPublish("", QUEUE_NAME, null, saved.getId().getBytes());
-            System.out.println(" [x] Sent '" + saved.getId()+ "'");
+            System.out.println(" [x] Sent '" + saved.getId() + "'");
         } catch (Exception e) {
             System.out.println(e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
-        return saved.getId();
+        return new ResponseEntity<>(saved, HttpStatus.OK);
     }
 
-    public List<Hit<Article>> search(ElasticsearchClient esClient, String index, String field, String query) throws IOException {
+    @GetMapping("/{collectionId}")
+    ResponseEntity<QOne> getOne(@PathVariable(value = "collectionId") String id) {
+        QOne qone = repo.findById(id).orElse(null);
+
+        if (qone == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(qone, HttpStatus.OK);
+
+    }
+
+    @GetMapping("/")
+    ResponseEntity<List<QOne>> getAll() {
+        return new ResponseEntity<>(repo.findAll(), HttpStatus.OK);
+    }
+
+    public List<Hit<Article>> search(ElasticsearchClient esClient, String index, String field, String query)
+            throws IOException {
 
         SearchResponse<Article> response = esClient.search(s -> s
-            .index(index)
-            .query(q -> q
-                .match(t -> t
-                    .field(field)
-                    .query(query)
-                )
-            ),
-            Article.class
-        );
+                .index(index)
+                .query(q -> q
+                        .match(t -> t
+                                .field(field)
+                                .query(query))),
+                Article.class);
 
         System.out.println("Searching done");
         return response.hits().hits();
     }
 
     @GetMapping("/{collectionId}/q2")
-    String get(@PathVariable(value = "collectionId") String id, @RequestParam String query, @RequestParam int k) {
+    ResponseEntity<QTwoTopics> get(@PathVariable(value = "collectionId") String id, @RequestParam String query,
+            @RequestParam int k) {
         // URL and API key
         String serverUrl = "http://elastic:9200";
 
         // Create the low-level client
         RestClient restClient = RestClient
-            .builder(HttpHost.create(serverUrl))
-            .build();
+                .builder(HttpHost.create(serverUrl))
+                .build();
 
         // Create the transport with a Jackson mapper
         ElasticsearchTransport transport = new RestClientTransport(
-            restClient, new JacksonJsonpMapper());
+                restClient, new JacksonJsonpMapper());
 
         // And create the API client
         ElasticsearchClient esClient = new ElasticsearchClient(transport);
@@ -171,6 +188,7 @@ public class QOneController {
             model.estimate();
         } catch (Exception e) {
             e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
         System.out.println("Execution finished");
@@ -178,7 +196,6 @@ public class QOneController {
         Alphabet dataAlphabet = instances.getDataAlphabet();
         ArrayList<TreeSet<IDSorter>> topicSortedWords = model.getSortedWords();
         ArrayList<ArrayList<String>> topics = new ArrayList<>();
-        ObjectMapper om = new ObjectMapper();
 
         for (int topic = 0; topic < numTopics; topic++) {
             Formatter out = new Formatter(new StringBuilder(), Locale.US);
@@ -190,21 +207,14 @@ public class QOneController {
             while (iterator.hasNext() && rank < 10) {
                 IDSorter idCountPair = iterator.next();
                 out.format("%s (%.0f) ", dataAlphabet.lookupObject(idCountPair.getID()), idCountPair.getWeight());
-                currentTopic.add((String)dataAlphabet.lookupObject(idCountPair.getID()));
+                currentTopic.add((String) dataAlphabet.lookupObject(idCountPair.getID()));
                 rank++;
             }
             topics.add(currentTopic);
             System.out.println(out);
         }
 
-        try {
-            QTwoTopics res = new QTwoTopics(topics);
-            return om.writeValueAsString(res);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-
-        return "Something went wrong";
+        return new ResponseEntity<>(new QTwoTopics(topics), HttpStatus.OK);
     }
-    
+
 }
