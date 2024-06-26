eed555c17da4cd8f4643601a0693e0e70fa3cdc7
Andrea Stocco <astocco11@gmail.com>
2024-06-26 17:08:45 +0200

    code: add delete endpoint

--- awesomemodeling/src/main/java/ama/awesomemodeling/controllers/QOneController.java
+++ awesomemodeling/src/main/java/ama/awesomemodeling/controllers/QOneController.java
@@ -8,14 +8,7 @@ import ama.awesomemodeling.services.ElasticService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
@@ -85,6 +78,18 @@ public class QOneController {
 
         return new ResponseEntity<>(qone, HttpStatus.OK);
 
+    }
+
+    @DeleteMapping("/{collectionId}")
+    ResponseEntity<QOne> deleteOne(@PathVariable(value = "collectionId") String id) {
+        QOne qone = repo.findById(id).orElse(null);
+
+        if (qone == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        repo.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("")
